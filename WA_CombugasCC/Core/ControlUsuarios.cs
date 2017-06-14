4c6959faedda787ce3a74b19c3363a6473774219
Eduardo Armendariz <earmendariz@awsoftware.mx>
2017-06-14 16:19:46 -0500

    Carga Inicial
    Carga Inicial de Proyecto con Master Page

---
+++ WA_CombugasCC/Core/ControlUsuarios.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WA_CombugasCC.Core
+{
+    public class ControlUsuarios
+    {
+
+        public static int LogIn(string userName, string password)
+        {
+            int result = 0;
+            usuario entity = null;
+
+            try
+            {
+                ContextCombugasDataContext model = new ContextCombugasDataContext();
+                entity = model.usuarios.Where(x => x.username == userName && x.passwords == password).SingleOrDefault();
+
+                if (entity != null)
+                {
+                    if (!entity.isActive)
+                    {
+                        
+                    }
+                    else
+                    {
+                        
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("Ha ocurrido un error al iniciar la sesión del usuario. " + ex.Message);
+            }
+
+            return result;
+        }
+
+    }
+}
